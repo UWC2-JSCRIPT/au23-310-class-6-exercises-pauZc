f7294c3ef1f8c6ffda203dac4ca37fb6ce040331
Laura Zanella <zclaurap@gmail.com>
2023-11-20 19:31:28 -0800

    to do list

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -2,18 +2,36 @@
  * Toggles "done" class on <li> element
  */
 
+$('li').click(function(){
+  $(this).addClass('done')
+})
 /**
  * Delete element when delete link clicked
  */
-
+$('.delete').click(function(){
+  $(this).parent().remove()
+})
 /**
  * Adds new list item to <ul>
  */
 const addListItem = function(e) {
   e.preventDefault();
-  const text = $('input').val();
+  const text = $('input').val()
+  var $li = $("<li>")
+  $li.click(function(){ $(this).addClass('done') })
 
-  // rest here...
+  var $span = $("<span>")
+  $span.text(text)
+
+  var $a = $("<a>", { "class": "delete"});
+  $a.text('Delete')
+ $a.click(function(){$(this).parent().remove()})
+
+  $li.append($span);
+  $li.append($a);
+
+  $('.today-list').append($li)
 };
 
 // add listener for add
+$('.add-item').click(addListItem)
